724e4081dde22506136812ee19c0aa5b27ef05b7
arnie b <arnold.bwaila@gmail.com>
2019-08-25 17:52:07 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,10 +8,10 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  potential_winners = []
+  winner = ""
   passengers.each do |key, value|
     if (key == suite_a && value.starts_with?("A"))
-      potential_winners << value
+      winner << value
     end
   end
 end
